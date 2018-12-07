37fb278ea3456962c6e9d627fb4885008811bba1
Maksim Strenk <ephdtrg@gmail.com>
2018-12-07 18:21:55 +0300

    improve code structure

--- test/DepositContract.js
+++ test/DepositContract.js
@@ -10,7 +10,7 @@ const truffleAssert  = require('truffle-assertions');
 
 const BigNumber = web3.BigNumber;
 
-const Token = artifacts.require('./TestToken.sol')
+const Token = artifacts.require('./TestToken.sol');
 const Whitelist = artifacts.require('./Whitelist.sol');
 const Vault = artifacts.require('./Vault.sol');
 const Silver = artifacts.require('./SilverDepositPlan.sol');
@@ -22,32 +22,33 @@ const DAY = 24 * 3600;
 const gasPrice = 10 ** 11;
 
 contract('DepositContract', accounts => {
-	const OWNER = accounts[1]
-	const INVESTORS = [accounts[2], accounts[3], accounts[4]];
+    const OWNER = accounts[1];
+    const INVESTORS = [accounts[2], accounts[3], accounts[4]];
+    const INVESTOR_1 = accounts[2];
 
-	let snapshotId;
+    let snapshotId;
 
-	const getBlockchainTimestamp = async () => {
+    const getBlockchainTimestamp = async () => {
         const latestBlock = await web3async(web3.eth, web3.eth.getBlock, 'latest');
         return latestBlock.timestamp;
     };
 
     const createDepositContracts = async () => {
-    	const token = await Token.new();
-    	const whitelist = await Whitelist.new(token.address);
-    	const silverContract = await Silver.new(token.address, whitelist.address);
-    	const goldContract = await Gold.new(token.address, whitelist.address);
-    	const platinumContract = await Platinum.new(token.address, whitelist.address);
+        const token = await Token.new();
+        const whitelist = await Whitelist.new(token.address);
+        const silverContract = await Silver.new(token.address, whitelist.address);
+        const goldContract = await Gold.new(token.address, whitelist.address);
+        const platinumContract = await Platinum.new(token.address, whitelist.address);
         const demoContract = await TryAndBuy.new(token.address, whitelist.address);
         return {
-        	token: token,
-        	whitelist: whitelist,
-        	silver: silverContract,
-        	gold: goldContract,
-        	platinum: platinumContract,
-        	demo: demoContract
-        }
-    }
+            token: token,
+            whitelist: whitelist,
+            silver: silverContract,
+            gold: goldContract,
+            platinum: platinumContract,
+            demo: demoContract
+        };
+    };
 
     beforeEach(async () => {
         snapshotId = (await snapshot()).result;
@@ -58,116 +59,129 @@ contract('DepositContract', accounts => {
     afterEach(async () => {
         await revert(snapshotId);
     });
-    
+
     describe('Precheck', async () => {
         it('#0 gas limit', async () => {
-        	const token = await Token.new();
-    	    await estimateConstructGas(Whitelist, OWNER, token.address)
-    	        .then(gas => console.info('      Whitelist construct gas:', gas));
-    	    const whitelist = await Whitelist.new(token.address)
-    	    await estimateConstructGas(Silver, OWNER, whitelist.address, token.address)
-    	        .then(gas => console.info('      Silver contract construct gas:', gas));
-    	    await estimateConstructGas(Gold, OWNER, whitelist.address, token.address)
-    	        .then(gas => console.info('      Gold contract construct gas:', gas));
-    	    await estimateConstructGas(Platinum, OWNER, whitelist.address, token.address)
-    	        .then(gas => console.info('      Platinum contract construct gas:', gas));
-    	    await estimateConstructGas(TryAndBuy, OWNER, whitelist.address, token.address)
-    	        .then(gas => console.info('      Try and buy contract construct gas:', gas));
-    	    await estimateConstructGas(Vault, OWNER, token.address)
-    	        .then(gas => console.info('      Vault contract construct gas:', gas))
-        })
+            const token = await Token.new();
+            await estimateConstructGas(Whitelist, OWNER, token.address)
+                .then(gas => console.info('      Whitelist construct gas:', gas));
+            const whitelist = await Whitelist.new(token.address);
+            await estimateConstructGas(Silver, OWNER, whitelist.address, token.address)
+                .then(gas => console.info('      Silver contract construct gas:', gas));
+            await estimateConstructGas(Gold, OWNER, whitelist.address, token.address)
+                .then(gas => console.info('      Gold contract construct gas:', gas));
+            await estimateConstructGas(Platinum, OWNER, whitelist.address, token.address)
+                .then(gas => console.info('      Platinum contract construct gas:', gas));
+            await estimateConstructGas(TryAndBuy, OWNER, whitelist.address, token.address)
+                .then(gas => console.info('      Try and buy contract construct gas:', gas));
+            await estimateConstructGas(Vault, OWNER, token.address)
+                .then(gas => console.info('      Vault contract construct gas:', gas));
+        });
 
         it('#1 create deposit contracts', async () => {
-    	    const depositContracts = await createDepositContracts();
-    	    depositContracts.token.address.should.have.length(42);
-    	    depositContracts.whitelist.address.should.have.length(42);
-    	    depositContracts.silver.address.should.have.length(42);
-    	    depositContracts.gold.address.should.have.length(42);
-    	    depositContracts.platinum.address.should.have.length(42);
-    	    depositContracts.demo.address.should.have.length(42);
-        })
-    })
+            const depositContracts = await createDepositContracts();
+            depositContracts.token.address.should.have.length(42);
+            depositContracts.whitelist.address.should.have.length(42);
+            depositContracts.silver.address.should.have.length(42);
+            depositContracts.gold.address.should.have.length(42);
+            depositContracts.platinum.address.should.have.length(42);
+            depositContracts.demo.address.should.have.length(42);
+        });
+    });
 
 describe('Whitelist', async () =>{
-	it('#1 check whitelist empty', async () => {
-		const depositContracts = await createDepositContracts();
-		const whitelist = depositContracts.whitelist;
-		const INVESTOR = INVESTORS[0];
-		const notWhitelisted = await whitelist.isWhitelisted(INVESTOR);
-		notWhitelisted.should.be.false;
-	})
-
-	it('#2 check add to whitelist', async () => {
-		const depositContracts = await createDepositContracts();
+    it('#1 check whitelist empty', async () => {
+        const depositContracts = await createDepositContracts();
         const whitelist = depositContracts.whitelist;
-        (await whitelist.isWhitelisted(INVESTORS[0])).should.be.false;
-        await whitelist.addAddressToWhitelist(INVESTORS[0]);
-        (await whitelist.isWhitelisted(INVESTORS[0])).should.be.true;
-	})
+        const notWhitelisted = await whitelist.isWhitelisted(INVESTOR_1);
+        notWhitelisted.should.be.false;
+    });
 
-	it('#3 check bulk add to whitelist', async () => {
-		const depositContracts = await createDepositContracts();
+    it('#2 check add to whitelist', async () => {
+        const depositContracts = await createDepositContracts();
         const whitelist = depositContracts.whitelist;
-        (await whitelist.isWhitelisted(INVESTORS[0])).should.be.false;
-        (await whitelist.isWhitelisted(INVESTORS[1])).should.be.false;
-        (await whitelist.isWhitelisted(INVESTORS[2])).should.be.false;
+        (await whitelist.isWhitelisted(INVESTOR_1)).should.be.false;
+        await whitelist.addAddressToWhitelist(INVESTOR_1);
+        (await whitelist.isWhitelisted(INVESTOR_1)).should.be.true;
+    });
+
+    it('#3 check bulk add to whitelist', async () => {
+        const depositContracts = await createDepositContracts();
+        const whitelist = depositContracts.whitelist;
+        for (let i = 0; i < INVESTORS.length; i++) {
+            (await whitelist.isWhitelisted(INVESTORS[i])).should.be.false;
+        }
         await whitelist.addAddressesToWhitelist(INVESTORS);
-        (await whitelist.isWhitelisted(INVESTORS[0])).should.be.true;
-        (await whitelist.isWhitelisted(INVESTORS[1])).should.be.true;
-        (await whitelist.isWhitelisted(INVESTORS[2])).should.be.true;
-	})
+        for (let i = 0; i < INVESTORS.length; i++) {
+            (await whitelist.isWhitelisted(INVESTORS[i])).should.be.true;
+        }
+    });
 
-	it('#4 check remove from whitelist', async () => {
-		const depositContracts = await createDepositContracts();
+    it('#4 check remove from whitelist', async () => {
+        const depositContracts = await createDepositContracts();
         const whitelist = depositContracts.whitelist;
-        await whitelist.addAddressToWhitelist(INVESTORS[0]);
-        (await whitelist.isWhitelisted(INVESTORS[0])).should.be.true;
-        await whitelist.removeAddressFromWhitelist(INVESTORS[0]);
-        (await whitelist.isWhitelisted(INVESTORS[0])).should.be.false;
-	})
+        await whitelist.addAddressToWhitelist(INVESTOR_1);
+        (await whitelist.isWhitelisted(INVESTOR_1)).should.be.true;
+        await whitelist.removeAddressFromWhitelist(INVESTOR_1);
+        (await whitelist.isWhitelisted(INVESTOR_1)).should.be.false;
+    });
 
-	it('#5 check bulk remove from whitelist', async () => {
-		const depositContracts = await createDepositContracts();
+    it('#5 check bulk remove from whitelist', async () => {
+        const depositContracts = await createDepositContracts();
         const whitelist = depositContracts.whitelist;
         await whitelist.addAddressesToWhitelist(INVESTORS);
-        (await whitelist.isWhitelisted(INVESTORS[0])).should.be.true;
-        (await whitelist.isWhitelisted(INVESTORS[1])).should.be.true;
-        (await whitelist.isWhitelisted(INVESTORS[2])).should.be.true;
+        for (let i = 0; i < INVESTORS.length; i++) {
+            (await whitelist.isWhitelisted(INVESTORS[i])).should.be.true;
+        }
         await whitelist.removeAddressesFromWhitelist(INVESTORS);
-        (await whitelist.isWhitelisted(INVESTORS[0])).should.be.false;
-        (await whitelist.isWhitelisted(INVESTORS[1])).should.be.false;
-        (await whitelist.isWhitelisted(INVESTORS[2])).should.be.false;
-	})
+        for (let i = 0; i < INVESTORS.length; i++) {
+            (await whitelist.isWhitelisted(INVESTORS[i])).should.be.false;
+        }
+    });
 
-	it('#6 check add events', async () => {
-		const depositContracts = await createDepositContracts();
-		const whitelist = depositContracts.whitelist;
-		const investor = INVESTORS[0];
-		const txAdd = await whitelist.addAddressToWhitelist(investor).should.be.fulfilled;
-		truffleAssert.eventEmitted(txAdd, 'WhitelistedAddressAdded', (ev) => {
-			return ev._address.should.be.equals(investor);
-		});
-		truffleAssert.prettyPrintEmittedEvents(txAdd);
-	})
+    it('#6 check add events', async () => {
+        const depositContracts = await createDepositContracts();
+        const whitelist = depositContracts.whitelist;
+        const txAdd = await whitelist.addAddressToWhitelist(INVESTOR_1).should.be.fulfilled;
+        truffleAssert.eventEmitted(txAdd, 'WhitelistedAddressAdded', (ev) => {
+            return ev._address.should.be.equals(INVESTOR_1);
+        });
+        truffleAssert.prettyPrintEmittedEvents(txAdd);
+    });
 
-	it('#7 check remove events', async () => {
-		const depositContracts = await createDepositContracts();
-		const whitelist = depositContracts.whitelist;
-		const investor = INVESTORS[0];
-		await whitelist.addAddressToWhitelist(investor).should.be.fulfilled;
-		const txRemove = await whitelist.removeAddressFromWhitelist(investor).should.be.fulfilled;
-		truffleAssert.eventEmitted(txRemove, 'WhitelistedAddressRemoved', (ev) => {
-			return ev._address.should.be.equals(investor);
-		});
-		truffleAssert.prettyPrintEmittedEvents(txRemove);
-	})
-})
+    it('#7 check remove events', async () => {
+        const depositContracts = await createDepositContracts();
+        const whitelist = depositContracts.whitelist;
+        await whitelist.addAddressToWhitelist(INVESTOR_1).should.be.fulfilled;
+        const txRemove = await whitelist.removeAddressFromWhitelist(INVESTOR_1).should.be.fulfilled;
+        truffleAssert.eventEmitted(txRemove, 'WhitelistedAddressRemoved', (ev) => {
+            return ev._address.should.be.equals(INVESTOR_1);
+        });
+        truffleAssert.prettyPrintEmittedEvents(txRemove);
+    });
+
+    it('#8 check cannot be added twice', async () => {
+        const depositContracts = await createDepositContracts();
+        const whitelist = depositContracts.whitelist;
+        await whitelist.addAddressToWhitelist(INVESTOR_1).should.be.fulfilled;
+        await whitelist.addAddressToWhitelist(INVESTOR_1).should.not.be.fulfilled;
+    });
+
+    it('#9 check cannot be removed twice', async () => {
+        const depositContracts = await createDepositContracts();
+        const whitelist = depositContracts.whitelist;
+        const INVESTOR_1 = INVESTORS[0];
+        await whitelist.addAddressToWhitelist(INVESTOR_1).should.be.fulfilled;
+        await whitelist.removeAddressFromWhitelist(INVESTOR_1).should.be.fulfilled;
+        await whitelist.removeAddressFromWhitelist(INVESTOR_1).should.not.be.fulfilled;
+    });
+});
 
 describe('Vault tests', async () =>{
-	it('#1 check accept tokens', async () => {
-		const token = await Token.new()
-		const vault = await Vault.new(OWNER, token.address)
-	})
-})
+    it('#1 check accept tokens', async () => {
+        const token = await Token.new();
+        const vault = await Vault.new(OWNER, token.address);
+    });
+});
 
 });
